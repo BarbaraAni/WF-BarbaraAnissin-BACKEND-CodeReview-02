dc007ec32577788085c99da1c9f8b3da5d960df7
Barbara Anissin <anissinb@gmail.com>
2019-11-28 18:32:29 +0100

    first commit

---
+++ src/Exercise.java
@@ -0,0 +1,14 @@
+public class Exercise {
+    //Exercise´s name
+    //Description
+    //Duration (in sec)
+    //Position (floor or standup)
+
+    //Using the text above, create 6 obj.
+    //put objs in array - for( bla x: blabla)
+    //sout Name \t duration \n ------ \n Description
+
+    //create new loop through array and only print:
+    //Exercises on floor
+    //Exercises on floor, that take more than 1min
+}
---
+++ src/IntervalWorkout.java
@@ -0,0 +1,17 @@
+public class IntervalWorkout {
+    //EXTRA
+    //Several Exercises
+    //Number of repetitions
+    //Breaktime (in seconds)
+
+    //example: Pushup Squat Plank - 4 times
+    //Output:
+    // Pushup, Squat and Plank, then a Break (duration: Breaktime),    then
+    //Pushup, Squat and Plank, then a Break (duration: Breaktime),    then
+    //Pushup, Squat and Plank, then a Break (duration: Breaktime),    then
+    //Pushup, Squat and Plank
+
+    //Print like this:
+    //****\nInterval Workout STARTS !!!!\n ***\n====\nROUND 1:....
+    //End: *****\nCONGRATS - YOU ARE DONE !!! \n ******
+}
